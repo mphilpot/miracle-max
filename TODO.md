fc7da9eaff0f5a0dd14d2e7803c704c4db275275
Michael Philpott <mphilpot@gmail.com>
2013-04-20 07:36:26 -0700

    added a default index file

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# MiracleMax
+
+A simple CMS based on express. Can be run seperately or bound into express.
+
+## Outstanding Issues
+
+[] Support template local values
+[] Support custom attributes
+[] Figure out a better testing scheme/template
---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports.Config = require('./lib/config');
+module.exports.Editor = require('./lib/config');
+module.exports.Preview = require('./lib/config');
+module.exports.Server = require('./lib/config');
+module.exports.SitemapRouter = require('./lib/config');
+
+
