853a27cdf79660465e129fc8edca9115f5e7a81e
Michael Philpott <mphilpot@gmail.com>
2013-07-12 07:08:18 -0700

    Updates preview to listen for sitemap changes.

--- test/sitemap-router-test.js
+++ test/sitemap-router-test.js
@@ -1,4 +1,8 @@
 var assert = require('assert');
+var nodeTemp = require('temp');
+var ncp = require('ncp');
+var path = require('path');
+var fs = require('fs');
 
 describe('sitemap-router.js', function () {
   var cwd = process.cwd();
@@ -8,12 +12,33 @@ describe('sitemap-router.js', function () {
     "/about": {}
   };
 
+  var config;
+
+  beforeEach(function (done) {
+    tempDir = nodeTemp.mkdirSync();
+
+    ncp(path.resolve(__dirname, 'res'), tempDir, function (err) {
+      process.chdir(tempDir);
+      config = require('../lib/config')();
+
+      done();
+    });
+  });
+
+  afterEach(function (done) {
+    process.chdir(cwd);
+    nodeTemp.cleanup();
+    done();
+  });
+
   it('verify object sitemap', function () {
 
-    var sitemapRouter = require('../lib/sitemap-router')(sitemap);
+    var sitemapRouter = require('../lib/sitemap-router')(config.sitemapFile);
     var paths = sitemapRouter.map.get.map(function(route) {
       return route.path;
     });
-    assert.deepEqual(Object.keys(sitemap), paths);
+    assert.deepEqual(Object.keys(config.sitemap), paths);
   });
+
+  // TODO(philpott): Write a test to verify sitemap update.
 });
